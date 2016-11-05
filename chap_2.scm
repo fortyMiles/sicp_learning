02292229162ab6c8913f5685e6fa9491d443c42b
fortymiles <mqgao@outlook.com>
2016-11-05 11:59:00 +0800

    finish filter and accumulate

--- chap_2.scm
+++ chap_2.scm
@@ -135,3 +135,20 @@
 			    (cons (car s) e))
 			  rest))))
   )
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+	((predicate (car sequence))
+	 (cons (car sequence)
+	       (filter predicate (cdr sequence))
+	       )
+	 )
+	(else (filter predicate (cdr sequence)))
+  ))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence) (accumulate op initial (cdr sequence)))
+      )
+  )
