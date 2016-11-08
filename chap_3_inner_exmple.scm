cd43146c159d36263a327556a117d2fdf6b456cc
fortymiles <mqgao@outlook.com>
2016-11-08 15:58:59 +0800

    account has an error

--- chap_3_inner_exmple.scm
+++ chap_3_inner_exmple.scm
@@ -5,35 +5,55 @@
 	       balance)
 	"insuficient founds")
 	)
-    )
-(define (make-account balance)
-  (define (withdraw amount)
-    (if (>= balance amount)
-	(begin (set! balance (- balance amount))
-	       balance
-	       )
-	"Insufficient founds"
-	)
-    )
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance
-    )
-  (define (dispatch m)
-    (cond ((eq? m 'withdraw) withdraw)
-	  ((eq? m 'deposit) deposit)
-	  (else (error "Unknow request: MAKE-ACCOUNT" m))
+  )
+(define (make-account balance password)
+  (let ((wrong-psd-time 0))
+    (define (withdraw amount)
+      (if (>= balance amount)
+	  (begin (set! balance (- balance amount))
+		 balance
+		 )
+	  "Insufficient founds"
 	  )
-    )
-  dispatch)
+      )
+
+    (define (deposit amount)
+      (set! balance (+ balance amount))
+      balance
+      )
+
+    (define (call-cops)
+      (error "call the police now!")
+      )
+
+    (define (wrong-password)
+      (begin
+	;(set! wrong-psd-time (+ wrong-psd-time 1))
+	     (display "incorrect password")
+	;     (call-cops)
+      ))
+
+    (define (access 'm)
+      (begin (set! wrong-psd-time 0)
+	     (cond ((eq? m 'withdraw) withdraw)
+		   ((eq? m 'deposit) deposit)
+		   (else (error "Unknow request: MAKE-ACCOUNT" m))
+		   )))
+
+    (define (dispatch psd m)
+      (if (not (eq? psd password))
+	  wrong-password
+	  (access 'm)))
+
+    dispatch))
 
 ;; 3.1 accumulator
 
 (define (accumulator initial)
   (lambda (augment)
     (begin (set! initial (+ initial augment))
-    initial)
-  ))
+	   initial)
+    ))
 
 ;; 3.2 
 
@@ -49,7 +69,7 @@
       (cond ((eq? arg 'how-many-calls?) called-time)
 	    ((eq? arg 'reset-count) (reset))
 	    (else (origin-proc (cons arg args)))
-	  )
+	    )
       )
     mf))
 
